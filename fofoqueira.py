2abcd3926584c2f4ea696b3ce44085816b8a0153
matheus.tavares <matheus.tavares@cwi.com.br>
2023-01-07 13:01:54 -0300

    fix: final version work image tts and price

--- fofoqueira.py
+++ fofoqueira.py
@@ -4,6 +4,7 @@ from decouple import config
 import requests
 import random
 import json
+import re
 
 client = commands.Bot(command_prefix='.', intents=discord.Intents.all())
 
@@ -18,8 +19,8 @@ async def on_message(message):
             # Conectar o bot ao canal de voz do autor da mensagem
             await message.author.voice.channel.connect()
     if message.content.startswith('.preco'):
-        obter_preco_jogo(message)
-        exibir_imagem(message)
+        await obter_preco_jogo(message)
+        await exibir_imagem(message)
 
 
 # <---------------------------------- TTS ------------------------------------------------------>
@@ -27,17 +28,19 @@ async def on_message(message):
 async def on_voice_state_update(member, before, after):
     # Verificar se o membro acabou de entrar em um canal de voz
     if before.channel is None and after.channel is not None:
+        channelNameHandler = remover_emojis(after.channel.name)
         # Enviar uma mensagem dizendo que o membro entrou no canal
-        await after.channel.send(f'{member.mention} entrou no canal {after.channel.name}', tts=True)
+        await after.channel.send(f'{member.mention} entrou no canal {channelNameHandler}', tts=True)
     # Verificar se o membro acabou de sair de um canal de voz
     elif before.channel is not None and after.channel is None:
+        channelNameHandler = remover_emojis(before.channel.name)
         # Enviar uma mensagem dizendo que o membro saiu do canal
-        await before.channel.send(f'{member.mention} saiu do canal {before.channel.name}', tts=True)
+        await before.channel.send(f'{member.mention} saiu do canal {channelNameHandler}', tts=True)
 
 
 # <---------------------------- PRECO DOS JOGOS STEAM ----------------------------------------->
 
-def obter_preco_jogo(message):
+async def obter_preco_jogo(message):
   nome_jogo = message.content[7:]
   # Substitua os espaços no nome do jogo por %20 para torná-lo compatível com a URL
   nome_jogo = nome_jogo.replace(' ', '%20')
@@ -61,8 +64,8 @@ def obter_preco_jogo(message):
       # Verifique se a função retornou um preço válido
       if price:
         # Envie o preço para o canal
-        await message.channel.send(price) 
-        #  await message.channel.send(f'- {respostaPrefix} {preco:.2f} {respostaSufix}') 
+        # await message.channel.send(price) 
+        await message.channel.send(f'- {respostaPrefix} {price:.2f} {respostaSufix}') 
       else:
         # Se a função retornou None, envie uma mensagem informando que o jogo não foi encontrado ou não possui um preço definido
         await message.channel.send(f'Não foi possível encontrar o preço de {nome_jogo}')
@@ -73,10 +76,14 @@ def obter_preco_jogo(message):
     # Se o jogo não foi encontrado, retorne None
     return None
 
+def remover_emojis(nome_canal):
+    # Remove os emojis do nome do canal usando expressões regulares
+    return re.sub(r'[^\w\s]', '', nome_canal)
+
 
 async def exibir_imagem(message):
   # Obtenha o nome do jogo da mensagem
-  nome_jogo = message.content[8:]
+  nome_jogo = message.content[7:]
   # Substitua os espaços no nome do jogo por %20 para torná-lo compatível com a URL
   nome_jogo = nome_jogo.replace(' ', '%20')
   # Envie uma solicitação à API do Steam para pesquisar o jogo
