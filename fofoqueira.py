97bb3d1503d6be590e7e0349d0829b202c3be549
Pedro Maracaja <pedroomrj@gmail.com>
2023-01-08 18:35:33 -0300

    feat: added valid command and system sales

--- fofoqueira.py
+++ fofoqueira.py
@@ -7,24 +7,56 @@ import json
 import re
 from datetime import datetime
 
-client = commands.Bot(command_prefix='.', intents=discord.Intents.all())
+client = commands.Bot(command_prefix='f!', intents=discord.Intents.all())
 
 flood_limit = 5
 voice_join_times = {}
+lista_de_vendas = []
+
+# Remover Help padrão
+client.remove_command('help')
+
+@client.command()
+async def vender(ctx, valor: float, *, produto: str):
+  author = ctx.message.author
+  venda = {'author': str(author), 'produto': produto, 'valor': valor}
+  lista_de_vendas.append(venda)
+  await ctx.send(f'O produto "{produto}" foi adicionado à lista de vendas no valor de R$ {valor}')
+
+@client.command()
+async def vendas(ctx):
+  if not lista_de_vendas:
+    await ctx.send('Não há vendas cadastradas')
+    return
+  response = '**Lista de vendas:**\n'
+  for venda in lista_de_vendas:
+    response += f'O produto {venda["produto"]} está por R$ {venda["valor"]}, o usuario ({venda["author"]} esta vendendo!)\n'
+  await ctx.send(response)
+
+@client.command()
+async def preco(ctx):
+  await obter_preco_jogo(ctx.message)
+  await exibir_imagem(ctx.message)
+
+@client.command()
+async def fofoqueira(ctx):
+  if ctx.message.author.voice is not None:
+    await ctx.message.author.voice.channel.connect()
+
+@client.command()
+async def help(ctx):
+  await show_commands(ctx.message)
 
 @client.event
-async def on_message(message):
-  if message.content.startswith(".fofoqueira"):
-    if message.author.voice is not None:
-      await message.author.voice.channel.connect()
-  if message.content.startswith('.preco'):
-    await obter_preco_jogo(message)
-    await exibir_imagem(message)
-  if message.author.top_role.name == "Sênior" and message.content == ".rollback-nicks":
-    for member in message.guild.members:
-      if member.nick is not None:
-        apelido_revertido = member.nick[::-1]
-        await member.edit(nick=apelido_revertido)
+async def on_command_error(ctx, error):
+  if isinstance(error, commands.CommandNotFound):
+    await ctx.message.channel.send("Parece que você não sabe me usar(UUUUiii)")
+    await ctx.send(file=discord.File('assets/la_ele.png'))
+    await show_commands(ctx.message)
+
+async def show_commands(message):
+  command_list = [command.name for command in client.commands]
+  await message.channel.send(f'Comandos disponíveis: {", ".join(command_list)}')
 
 # <---------------------------------- TTS ------------------------------------------------------>
 @client.event
@@ -37,8 +69,7 @@ async def on_voice_state_update(member, before, after):
         await member.send('Você está entrando no canal de voz com uma frequência maior do que o permitido. Por favor, aguarde alguns segundos antes de entrar novamente.')
         await member.edit(nick="Flodador")
         return
-    else:
-      await after.channel.send(f'{member.mention} entrou no canal {remover_emojis(after.channel.name)}', tts=True)
+    await after.channel.send(f'{member.mention} entrou no canal {remover_emojis(after.channel.name)}', tts=True)
     voice_join_times[member.id] = now
   elif before.channel is not None and after.channel is None:
     await before.channel.send(f'{member.mention} saiu do canal {remover_emojis(before.channel.name)}', tts=True)
@@ -63,7 +94,7 @@ async def obter_preco_jogo(message):
       else:
         await message.channel.send(f'Não foi possível encontrar o preço de {nome_jogo}')
     else:
-      return None
+      await message.channel.send(f'Este jogo está gratis na steam!')
   else:
     return None
 
