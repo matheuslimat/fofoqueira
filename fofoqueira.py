0eb928ccf9ef4aa23ffa5d02528c1e29da2aca31
Pedro Maracaja <pedroomrj@gmail.com>
2023-03-28 18:59:33 -0300

    fix: serverId

--- fofoqueira.py
+++ fofoqueira.py
@@ -97,7 +97,7 @@ async def add_channel_twitch(ctx, valor: str):
     
     server_id = ctx.guild.id
 
-    twitchChannel.update_one({"servidorId": server_id}, {"$addToSet": {"canais": {
+    twitchChannel.update_one({"servidorId": str(server_id)}, {"$addToSet": {"canais": {
       "login": {valor},
       "status": False,
       "mensagemEntrada": f"O(a) {valor} está online. Assista em https://www.twitch.tv/{valor}",
